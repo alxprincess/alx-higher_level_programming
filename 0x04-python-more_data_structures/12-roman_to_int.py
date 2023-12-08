c6a442ac0a1f8c43f9d452a74e39042584dd43be
alxprincess <akuwudikeprincess@gmail.com>
2023-12-08 09:25:43 -0800

    alx-higher_level_programming

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,16 +3,16 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_num_list = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                   'C': 100, 'D': 500, 'M': 1000}
     result = 0
 
     for i in range(len(roman_string)):
-        current_value = roman_num_list[roman_string[i]]
+        current_value = roman_dict[roman_string[i]]
 
         if i < len(roman_string) - 1:
-            nxtup_value = roman_num_list[roman_string[i + 1]]
-            if current_value < nxtup_value:
+            next_value = roman_dict[roman_string[i + 1]]
+            if current_value < next_value:
                 result -= current_value
             else:
                 result += current_value

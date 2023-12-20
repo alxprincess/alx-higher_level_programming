4982f160755c32d3217f094efeecfe0d385eb9cf
alxprincess <akuwudikeprincess@gmail.com>
2023-12-19 18:09:14 -0800

    alx-higher_level_programming

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,17 +7,26 @@ class Square:
     """Represents a square."""
 
     def __init__(self, size=0):
-        """Initializes a new square.
+        """Initialize a new square.
 
         Args:
             size (int): The size of the new square.
         """
-        if not isinstance(size, int):
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
+        """Returns the current area of the square."""
         return (self.__size * self.__size)
